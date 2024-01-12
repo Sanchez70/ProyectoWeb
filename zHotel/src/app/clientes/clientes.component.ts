0db77aa078a563c8c0ed397a1fd41874877ec7af
Santana <carsantana54@gmail.com>
2024-01-12 10:45:45 -0500

    .

--- zHotel/src/app/clientes/clientes.component.ts
+++ zHotel/src/app/clientes/clientes.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
 
