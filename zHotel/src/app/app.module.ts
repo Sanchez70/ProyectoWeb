62c85250de4c2c9b37e4df0878e7f6d03432c85c
Santana <carsantana54@gmail.com>
2024-01-12 11:05:53 -0500

    Aplicando services en app module

--- zHotel/src/app/app.module.ts
+++ zHotel/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { RouterModule,Routes } from '@angular/router';
 import { ReservasComponent } from './reservas/reservas.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { FormClienteComponent } from './clientes/form-cliente.component';
+import { FormsModule } from '@angular/forms';
+import { ClienteService } from './clientes/cliente.service';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {path: '', redirectTo: '', pathMatch: 'full'}
@@ -27,9 +31,12 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
+    ClienteService,
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
