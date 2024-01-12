486f6f95b5240905e99de57c59584f99e22af1cf
Jean15M <127127163+Jean15M@users.noreply.github.com>
2024-01-12 13:04:51 -0500

    DISEÑO DEL FORMS RESERVA 10%
    Se esta diseñando el form de reserva.

--- zHotel/src/app/reservas/form-reservas.component.ts
+++ zHotel/src/app/reservas/form-reservas.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Reserva } from './reserva';
+import { ReservaService } from './reserva.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-form-reservas',
   templateUrl: './form-reservas.component.html',
 })
-export class FormReservasComponent {
+export class FormReservasComponent implements OnInit {
+  public reserva:Reserva = new Reserva()
+  public titulo:string="Crear Cliente"
+  constructor(private reservaService:ReservaService, private router:Router,
+    private activatedRoute:ActivatedRoute){}
 
+  ngOnInit(): void {
+    
+  }
+
+  public create():void{
+    this.reservaService.create(this.reserva).subscribe(
+        response => {
+          this.router.navigate(['/clientes'])
+          //Swal.fire('Cliente guardado',`Reserva guardada con éxito`,'success')
+    }
+    );
+  }
 }
