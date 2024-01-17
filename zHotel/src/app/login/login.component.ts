97d36b169cbcd9b5832da32c648ff971f08bbf09
Alewho777 <4lej4ndr0flor3s69@gmail.com>
2024-01-17 07:15:27 -0500

    css

--- zHotel/src/app/login/login.component.ts
+++ zHotel/src/app/login/login.component.ts
@@ -12,6 +12,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  //styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
