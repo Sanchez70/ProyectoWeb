bacefb01262f5420740b46c1baff59d185d7b204
Jean15M <127127163+Jean15M@users.noreply.github.com>
2024-01-23 14:03:19 -0500

    PANTALLA RESERVAS 20%
    Se esta realizando la pantalla reservas.

--- zHotel/src/app/reservas/form-reservas.component.ts
+++ zHotel/src/app/reservas/form-reservas.component.ts
@@ -8,27 +8,37 @@ import { EncabezadoFactura } from './encabezado-factura';
 import { EncabezadoFacturaService } from './encabezado-factura.service';
 import { DetalleFactura } from './detalle-factura';
 import { DetalleFacturaService } from './detalle-factura.service';
+import { AppComponent } from '../app.component';
 import Swal from 'sweetalert2';
+import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-form-reservas',
   templateUrl: './form-reservas.component.html',
 })
 export class FormReservasComponent implements OnInit {
+  idCliente:number = this.inicio.idUsuario;
   fechaEntrada: string='';
   fechaSalida: string='';
   diferenciadias: number = 0;
-  habitaciones2 = {precio:0};
+  idReserva:number=0;
+  idEncabezado:number=0;
+  idHabitaciones:number=0;
+  metodoPago:number=0;
   dias : number =0;
   total: number =0;
+  opciones: number[] = [1,2,3,4];
+  opcionSeleccionada: number=0;
+  
   public reserva:Reserva = new Reserva()
   public encabezado:EncabezadoFactura = new EncabezadoFactura()
   public detalle:DetalleFactura = new DetalleFactura()
   public habitaciones: Habitaciones = new Habitaciones()
   constructor(private reservaService:ReservaService, private habitacionesService:HabitacionesService,private encabezadoService:EncabezadoFacturaService,private detalleService:DetalleFacturaService ,private router:Router,
-    private activatedRoute:ActivatedRoute){}
+    private activatedRoute:ActivatedRoute,private inicio: AppComponent){}
 
   ngOnInit(): void {
     this.cargarhabitacion()
+    console.log('Id cliente en reserva',this.idCliente);
   }
 
   cargarhabitacion(): void {
@@ -36,27 +46,21 @@ export class FormReservasComponent implements OnInit {
       let id = params['id']
       if (id) {
         this.habitacionesService.getHabitacionesid(id).subscribe((habitacion) => this.habitaciones = habitacion)
+        this.reserva.idHabitaciones=this.habitaciones.idHabitaciones;
+        this.reserva.idCliente=this.idCliente;
       }
     })
+
   }
 
-  public create():void{
+  create(){
     this.reservaService.create(this.reserva).subscribe(
-        response => {
-          this.encabezadoService.create(this.encabezado).subscribe(
-            response=>{
-              this.detalleService.create(this.detalle).subscribe(
-                response=>{
-                  this.router.navigate(['/habitaciones'])
-                  Swal.fire('Reserva realizada',`Reserva realizada con éxito`,'success')
-                }
-              );
-            }
-          );
-          
-    }
-    );
- 
+      response => {
+        this.router.navigate(['/habitaciones'])
+        Swal.fire('Proveedor guardado',`guardado con éxito`,'success')
+  }
+  );
+  console.log(this.reservaService.create(this.reserva));
   }
 
   calcularDiferenciaDeDias() {
@@ -71,10 +75,10 @@ export class FormReservasComponent implements OnInit {
     }
   }
 
-  calcularTotal(){
-    const precio=this.habitaciones2.precio;
-    console.log(this.habitaciones2.precio)
-    this.total=precio*this.dias;
-    console.log('Este es el total',this.total);
+  calcularTotal(precio : number){
+      this.total = precio*this.diferenciadias;
+      console.log(precio)
+      console.log('TOTAL',this.total)
+    
   }
 }
