b752ef7df239d31ad540dc8a8b921c979bfa3429
Alewho777 <4lej4ndr0flor3s69@gmail.com>
2024-01-26 11:37:41 -0500

    registrofoto

--- zHotel/src/app/registroC/formRC.component.ts
+++ zHotel/src/app/registroC/formRC.component.ts
@@ -14,7 +14,8 @@ import Swal from 'sweetalert2';
 export class FormRCComponent implements OnInit {
   @Input() formRC!: FormGroup;
   public cedulaPersona: string = '';
-  public registroC: RegistroC = new RegistroC();  
+  public registroC: RegistroC = new RegistroC();
+  previewImage: string | ArrayBuffer = '';  
 
   constructor(
     private fb: FormBuilder,
@@ -34,7 +35,7 @@ export class FormRCComponent implements OnInit {
     this.formRC = this.fb.group({
       usuario: ['', Validators.required],
       contrasena: ['', Validators.required],
-      //foto: ['', Validators.required],
+      foto: ['', Validators.required],
     });
   }
 
@@ -42,31 +43,33 @@ export class FormRCComponent implements OnInit {
   //   this.registroC = this.formRC.value;
   //   this.registroC.cedula_persona = this.cedulaPersona;
   //   this.registroCService.registrarCliente(this.registroC).subscribe(
-  //     () => {
-  //       Swal.fire('Registro exitoso', 'success');
-        
-  //       this.router.navigate(['/']); 
-  //     },
-  //     (error) => {
-  //       console.error('Error al registrar cliente:', error);
-  //       Swal.fire('Error', 'Ocurrió un error al intentar registrar sus datos', 'error');
-  //     }
+  //      () => {
+  //        Swal.fire('Registro exitoso', 'success');
+  //        this.router.navigate(['/']); 
+  //      },
+  //      (error) => {
+  //        console.error('Error al registrar cliente:', error);
+  //        Swal.fire('Error', 'Ocurrió un error al intentar registrar sus datos', 'error');
+  //      }
   //   );
-  // }
+  //  }
 
   registrarCliente(): void {
+    // Validar si se ha seleccionado una imagen
     if (!this.previewImage) {
       Swal.fire('Error', 'Por favor, seleccione una imagen', 'error');
       return;
     }
-  
+
     // Asignar la foto antes de enviarla al servicio
     const base64String = this.previewImage.toString();
     this.registroC.foto = base64String;
-  
+
+    // Asignar otros valores del formulario
     this.registroC = this.formRC.value;
     this.registroC.cedula_persona = this.cedulaPersona;
-  
+
+    // Llamar al servicio para registrar al cliente
     this.registroCService.registrarCliente(this.registroC).subscribe(
       () => {
         Swal.fire('Registro exitoso', 'success');
@@ -80,32 +83,23 @@ export class FormRCComponent implements OnInit {
   }
    
 
-  imagenSeleccionada: any;
-
   onFileSelected(event: any): void {
     const file: File = event.target.files[0];
     const reader = new FileReader();
 
     reader.onload = (e: any) => {
       this.previewImage = e.target.result;
-      this.registroC.foto = this.previewImage.toString();
     };
 
     reader.readAsDataURL(file);
   }
-  previewImage: string | ArrayBuffer = '';
 
-  imageRequiredValidator(control: any): { [key: string]: any } | null {
-    const value = control.value;
-  
-    if (value instanceof FileList && value.length > 0) {
-      return null; // Imagen seleccionada, la validación pasa
-    }
-  
-    if (typeof value === 'string' && value !== '') {
-      return null; // Dirección de imagen proporcionada, la validación pasa
-    }
-  
-    return { 'imageRequired': true }; // Falla la validación si no se proporciona una imagen
+  convertToBase64(): void {
+    if (this.previewImage) {
+      const base64String = this.previewImage.toString();
+      console.log(base64String);
+
+      this.registroC.foto = base64String;
+    } 
   }
 }
